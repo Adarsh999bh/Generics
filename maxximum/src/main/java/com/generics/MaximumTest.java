9e88445af641252b5e71bb47c3b18cec26fb4737
Adarsh999bh <adarshbhandary48@gmail.com>
2021-09-03 02:43:17 +0530

    maximum generics code added

---
+++ maxximum/src/main/java/com/generics/MaximumTest.java
@@ -0,0 +1,63 @@
+package com.generics;
+
+public class MaximumTest<T extends Comparable<T>> 
+{
+	
+	//class variables
+	 T x,y,z,w;  
+	
+	//parameterized construuctors for 3 variables
+	public MaximumTest(T x,T y,T z) {
+		this.x=x;
+		this.y=y;
+		this.z=z;
+	}
+	
+	//parameterized construuctors for 4 variables
+	public MaximumTest(T x,T y,T z,T w) {
+		this.x=x;
+		this.y=y;
+		this.z=z;
+		this.w=w;
+	}
+	
+	
+	//compare to function for 3 parameters
+	 public static <T extends Comparable<T>> T maximum(T x,T y,T z) {
+		 T max=x;     //will assume x is maximum then compare with y and z
+		 if(y.compareTo(max)>0) {
+			 max=y;
+		 }	
+		 if(z.compareTo(max)>0) {
+			 max=z;
+		 }
+		 printMax(x,y,z,max);
+		 return max;
+	 }
+	 //compare to function for 4 parameters
+	 public static <T extends Comparable<T>> T maximum(T x,T y,T z,T w) {
+		 T max=x;     //will assume x is maximum then compare with y and z
+		 if(y.compareTo(max)>0) {
+			 max=y;
+		 }
+		 if(z.compareTo(max)>0) {
+			 max=z;
+		 }
+		 if(w.compareTo(max)>0)
+		 {
+			 max=w;
+		 }
+		 printMax(x,y,z,w,max);
+		 return max;
+	 }
+	//to print the maximum value for 3 parameters to std out using genric method
+	public static <T> void printMax(T x,T y,T z,T max)
+	{
+		System.out.printf("Max of %s, %s and %s is %s\n",x,y,z,max);
+	}
+	//to print the maximum value for 4 parameters to std out using genric method
+	public static <T> void printMax(T x,T y,T z,T w,T max)
+	{
+		System.out.printf("Max of %s, %s, %s and %s is %s\n",x,y,z,w,max);
+	}
+}
